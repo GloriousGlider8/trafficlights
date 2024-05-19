1c08e7d33db7ff8d9eeb26920c0fcae1a19c3558
Sam <sam.g.plimmer@gmail.com>
2024-05-19 13:11:47 +0100

    Update

--- setup.py
+++ setup.py
@@ -11,11 +11,5 @@ setup(
     author_email="sam.g.plimmer@gmail.com",
     description="A package to help prevent race conditions.",
     packages=find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.8',
     long_description=getFile("README.md")
 )
